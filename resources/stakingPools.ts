9ab4bf15dddb9a413ef3462796263905baa1e3a8
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2022-08-11 18:19:51 +0200

    update plutus script and pool

--- resources/stakingPools.ts
+++ resources/stakingPools.ts
@@ -1,61 +1,63 @@
-import {poloPool} from './plutus.js'
+import * as scripts from './plutus.js'
 
-const poloPool1 = {
+const poloPool = {
     userInfo: {
-        rewardType: '$tPOLO',
+        rewardType: '$zPOLO',
     },
     poolInfo: {
-        name: "LP for $tPOLO",
+        name: "newLP for $zPolo",
         poolIndex: 0,
         imageSrc: "/tang.png",
-        stakingUnit: "29d91858c31d926d79c56de2f1cb59372d0f9da911ae388e3fd1ccc04c50",
-        harvestUnit: "a5042af9f2fa55ee9cf5480b990041bd2531772738c4b47aab78b8e074504f4c4f",
-        distAddress: "addr1qxw9nz43pplzhf6e2pf72lreldn7p4p6vr9y7m85uaahj4qzp7w0era4y6vtvhts8cjs3060gkeznfnzeg6p8pgptnnsz5tyf7",
-        script: poloPool,
+        contractAddress: "addr1wyy4wdd3fw833tv5my4g8h6klvyaq5gjsapvna4x4plwe0csk7av9",
+        stakingUnit: "cf85feeae10e8f6d36d265e2310045a4afc38319a779232e5dfff2466e65774c50",
+        harvestUnit: "6bc8b66cf43d277baec97024553c25f9c788c1c47eee54a6df07e6ed7a506f6c6f",
+        distAddress: "addr1qx07qk8l5q33qhgf02jcgxypx2emz0es8jau889ywmc2zsgzp7w0era4y6vtvhts8cjs3060gkeznfnzeg6p8pgptnnsgynp7z",
+        script: scripts.poloPool,
+	    oldScript: scripts.poloPool2,
         type: "FT",
         stakingPolicy: undefined,
         maxSize: 10000000,
-        rewardPerEpochQt: 1000000,
-        reward: "$tPOLO/epoch",
+        rewardPerEpochQt: 6000000,
+        reward: "$zPOLO/epoch",
         closesAt: new Date(new Date().getDay() + 365),
         openFrom: new Date()
     }
 }
 
 
-// const adao2PoolInfo = {
-//     userInfo: {
-//         rewardType: '$OAether',
-//     },
-//     poolInfo: {
-//         name: "ADAO for Origin Aether",
-//         poolIndex: 1,
-//                     //Policy Id of ADAO: 5b01968867e13432afaa2f814e1d15e332d6cd0aa77e350972b0967d
-//                     // Asset Name: ADAOGovernanceToken 4144414f476f7665726e616e6365546f6b656e
-//                     // both are together the staking unit
-//         stakingUnit: "5b01968867e13432afaa2f814e1d15e332d6cd0aa77e350972b0967d4144414f476f7665726e616e6365546f6b656e",
+const adao2PoolInfo = {
+    userInfo: {
+        rewardType: '$OAether',
+    },
+    poolInfo: {
+        name: "ADAO for Origin Aether",
+        poolIndex: 1,
+                    //Policy Id of ADAO: 5b01968867e13432afaa2f814e1d15e332d6cd0aa77e350972b0967d
+                    // Asset Name: ADAOGovernanceToken 4144414f476f7665726e616e6365546f6b656e
+                    // both are together the staking unit
+        stakingUnit: "5b01968867e13432afaa2f814e1d15e332d6cd0aa77e350972b0967d4144414f476f7665726e616e6365546f6b656e",
 
-//                     //Policy Id of Aether: faea3fac6594c1ca1a08c26ffffe6a3e7fb512a0e84786a26b9d836c
-//                     //Asset Name originAether 6f726967696e416574686572
-//                     // both are together the harvest unit
+                    //Policy Id of Aether: faea3fac6594c1ca1a08c26ffffe6a3e7fb512a0e84786a26b9d836c
+                    //Asset Name originAether 6f726967696e416574686572
+                    // both are together the harvest unit
 
-//         harvestUnit: "faea3fac6594c1ca1a08c26ffffe6a3e7fb512a0e84786a26b9d836c6f726967696e416574686572",
+        harvestUnit: "faea3fac6594c1ca1a08c26ffffe6a3e7fb512a0e84786a26b9d836c6f726967696e416574686572",
 
-//         distAddress: "addr1q8g59w6mtcnperwy5yq7fg3g547f9vjq8709wj3xk0w28573g2a4kh3xrjxufggpuj3z3ftuj2eyq0u72a9zdv7u50fssflav4",
-//         script: scripts.pool2v2,
-// 	oldScript: scripts.pool2,
-//         type: "FT",
-//         stakingPolicy: undefined,
-//         imageSrc: "/origin.png",
-//         maxSize: 10000000,
-//         rewardPerEpochQt: 40000000000,
-//         reward: "$OAether/epoch",
-//         closesAt: new Date(new Date().getDay() + 365),
-//         openFrom: new Date()
-//     }
-// }
+        distAddress: "addr1q8g59w6mtcnperwy5yq7fg3g547f9vjq8709wj3xk0w28573g2a4kh3xrjxufggpuj3z3ftuj2eyq0u72a9zdv7u50fssflav4",
+        script: scripts.pool2v2,
+	    oldScript: scripts.pool2,
+        type: "FT",
+        stakingPolicy: undefined,
+        imageSrc: "/origin.png",
+        maxSize: 10000000,
+        rewardPerEpochQt: 40000000000,
+        reward: "$OAether/epoch",
+        closesAt: new Date(new Date().getDay() + 365),
+        openFrom: new Date()
+    }
+}
 
-const stakingpools = [poloPool1]
+const stakingpools = [poloPool]
 
 
 export default stakingpools
